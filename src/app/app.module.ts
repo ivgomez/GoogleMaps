299a766a107500a053401e53067d88d702efeddf
Ivan Dario <iv.gomez@hotmail.com>
2018-10-26 16:07:18 -0500

    fourth commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppComponent } from './app.component';
 
 
 //angular material
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+import { MaterialModule } from './material.module';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { MapaEditarComponent } from './components/mapa/mapa-editar.component';
 
-import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
+  entryComponents:[
+    MapaEditarComponent
+  ],
+
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent,
+    MapaEditarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule
+    MaterialModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBXYBMAQUxsqk0YiYdQAISUoyMjv5nNtpE'
+      
+    })   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/classes/marcador.class.ts
@@ -0,0 +1,25 @@
+// export class Marcador{
+
+// constructor( 
+//         public lat: number, 
+//         public lng: number){
+// }
+//}
+
+export class Marcador{
+
+    public lat: number;
+    public lng: number;
+
+    public titulo = 'Sin titulo';
+    public desc = 'Sin descripcion';
+
+constructor( 
+    lat: number, 
+    lng: number){
+    
+    this.lat = lat;
+    this.lng = lng;
+}
+
+}
